ea855e2ed93052ec49d02624b65eca649023dfb0
Alsona Natsi <alsona.naci1188@gmail.com>
2024-07-12 07:04:50 +0000

    Functional greeting with props

---
+++ my-app/src/components/FuncionalGreetingWithProps.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+const FunctionalGreetingWithProps = (props) => {
+    return <h1>Hello {props.name}! I see you are {props.age} years old; {props.greeting}!!!</h1>;
+}
+export default FunctionalGreetingWithProps
